56bc1e924c52e24e1cb3f2af0ab3ac74f0721cdd
LaughingLeader <laughingleader@gmail.com>
2023-05-04 10:46:52 -0500

    Wrap path

--- Tools/generate-eocapp-symbols.bat
+++ Tools/generate-eocapp-symbols.bat
@@ -1,4 +1,4 @@
 @echo off
 set %SR%= & call get-solution-dir.bat %SR%
 
-%SR%\x64\Release\SymbolTableGenerator.exe "%~dp0\EoCApp.pdb" "%SR%\ScriptExtender\GameHooks\EoCApp.symtab"
+"%SR%\x64\Release\SymbolTableGenerator.exe" "%~dp0\EoCApp.pdb" "%SR%\ScriptExtender\GameHooks\EoCApp.symtab"
